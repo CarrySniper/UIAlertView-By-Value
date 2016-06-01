8cf00acd9df916dd4cb587761d7383ad5e718b80
cjq002 <seejoys@sijiukejideMac-mini.local>
2016-06-01 11:42:06 +0800

    修改配置

--- UIAlertView传值/ViewController.m
+++ UIAlertView传值/ViewController.m
@@ -23,18 +23,18 @@ static char alertInfoKey;
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    //初始化
+    // 3、初始化
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"标题"
                                                     message:@"内容消息"
                                                    delegate:self
                                           cancelButtonTitle:@"取消"
                                           otherButtonTitles:@"确定", nil];
     
-    //显示
+    // 4、显示
     [alert show];
     
     
-    //定义要传递的值
+    // 5、定义要传递的值
     NSString *passByValue = @"这是一个字符串";
     objc_setAssociatedObject(
                              alert,                     //对象
@@ -45,11 +45,18 @@ static char alertInfoKey;
 }
 
 #pragma mark - UIAlertViewDelegate
+/**
+ *  6、实现UIAlertViewDelegate方法
+ *
+ *  @param alertView   UIAlertView对象
+ *  @param buttonIndex 选中下标
+ */
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
 {
     if (buttonIndex == alertView.cancelButtonIndex) {
         NSLog(@"点击取消");
     }else{
+        // 6、取值
         NSString *getValue = objc_getAssociatedObject(
                                                       alertView,        //对象
                                                       &alertInfoKey     //键的引用
@@ -57,19 +64,6 @@ static char alertInfoKey;
         NSLog(@"取值：%@",getValue);
     }
 }
-
-
-//适配iPad iOS8 —— 替换 actionSheet: clickedButtonAtIndex:
-//- (void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex
-//{
-//    if (buttonIndex == uia) {
-//        
-//    }else{
-//        NSString *getValue = objc_getAssociatedObject(alertView, &alertInfoKey);
-//        NSLog(@"%@",data);
-//    }
-//        
-//}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
